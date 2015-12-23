8ed3455522dcb44946e9cc932029917ced198f51
abunuwas <joseharoperalta@gmail.com>
2015-12-23 13:35:49 +0000

    Add some changes to the model for the products application to ensure it works as expected

--- products/models.py
+++ products/models.py
@@ -1,16 +1,24 @@
 from django.db import models
 
 class Seller(models.Model):
-	name = models.CharField(max_length=500)
-	address = models.CharField(max_length=200)
-	email = models.EmailField(max_length=100)
-	password = models.CharField(max_length=100)
+	name = models.CharField(max_length=100)
+	address = models.CharField(max_length=100)
+	email = models.EmailField(max_length=50)
+	password = models.CharField(max_length=10)
+
+	def __str__(self):
+		return self.email
 
 class Product(models.Model):
 	seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
-	category = models.CharField(max_length=500)
+	title = models.CharField(max_length=150)
+	category = models.CharField(max_length=100)
 	geolocation = Seller(pk=seller).address
 	price = models.IntegerField(default=0)
-	sold = models.BooleanField(default=True)
+	sold = models.BooleanField(default=False)
 	description = models.TextField(max_length=1000)
+	created = models.DateTimeField('date created')
+
+	def __str__(self):
+		return self.title
 
