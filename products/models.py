9d78567ae9cb85d4bc2ce22124547edce1512837
abunuwas <joseharoperalta@gmail.com>
2015-12-23 12:32:40 +0000

    Create a first model for the products app

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Seller(models.Model):
+	name = models.CharField(max_length=500)
+	address = models.CharField(max_length=200)
+	email = models.EmailField(max_length=100)
+	password = models.CharField(max_length=100)
+
+class Product(models.Model):
+	seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+	category = models.CharField(max_length=500)
+	geolocation = Seller(pk=seller).address
+	price = models.IntegerField(default=0)
+	sold = models.BooleanField(default=True)
+	description = models.TextField(max_length=1000)
+
