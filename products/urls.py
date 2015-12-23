58eb4bdeab1d8017e8a832c5442cdb95bc5cd40c
abunuwas <joseharoperalta@gmail.com>
2015-12-23 16:12:41 +0000

    Wire the views with their respective urls in the products app

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,14 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^filter/(?P<productFilter>\w+)/$', views.filterProducts, name='filterProducts'),
+    url(r'^product/(?P<product_id>[0-9]+)/$', views.detailProduct, name='detailProduct'),
+    url(r'^buy/(?P<product_id>[0-9]+)/$', views.buyProduct, name='buyProduct'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.logIn, name='logIn'),
+    url(r'^loggedin/$', views.loggedIn, name='loggedIn'),
+    url(r'^loggedin/items/$', views.register, name='register'),
+    url(r'^loggedin/filter/(?P<productFilter>\w+)/$', views.filterSellerItems, name='filterSellerItems'),
+    url(r'^loggedin/filter/(?P<product_id>[0-9]+)/$', views.detailSellerProduct, name='detailSellerProduct'),
+    url(r'^loggedin/addproduct/$', views.addProduct, name='addProduct'),
     ]
