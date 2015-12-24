8c99529070a9e9e4029fbb5631222463672ae980
Jose <joseharoperalta@gmail.com>
2015-12-24 02:01:12 +0100

    Change the data model to link the Seller class to the User class from django.contrib.auth for authentication purposes

--- products/models.py
+++ products/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Seller(models.Model):
-	name = models.CharField(max_length=100)
+	user = models.OneToOneField(
+		User,
+		on_delete=models.CASCADE,
+		primary_key=True
+		)
 	address = models.CharField(max_length=100)
-	email = models.EmailField(max_length=50)
-	password = models.CharField(max_length=10)
 
 	def __str__(self):
-		return self.email
+		return self.user.email
 
 class Product(models.Model):
 	seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
