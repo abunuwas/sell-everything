6fadb397834cf8909e90fc136289f82e82a4e980
Jose <joseharoperalta@gmail.com>
2015-12-24 02:02:00 +0100

    Namespace the products application in the root urls.py file

--- classifiedAds/urls.py
+++ classifiedAds/urls.py
@@ -18,6 +18,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^products/', include('products.urls')),
+    url(r'^products/', include('products.urls', namespace='products')),
     url(r'^admin/', admin.site.urls),
 ]
