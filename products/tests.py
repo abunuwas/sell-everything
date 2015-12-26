5b964a6f155f7de2061fdb41e26c855cace5399a
abunuwas <joseharoperalta@gmail.com>
2015-12-26 23:54:23 +0100

    Start building tests for user creation and authentication

--- products/tests.py
+++ products/tests.py
@@ -5,7 +5,27 @@ from django.core.urlresolvers import reverse
 from .models import Seller, User, Product
 
 
-class registerUser(TestCase):
+class CreateSellerTest(TestCase):
+	def setUp(self):
+		jesus = User(first_name='Jesus', 
+					 last_name='Christ', 
+					 username='jesus',
+					 email='jesus@jesus.com',
+					 password='asdf1234')
+		jesus.set_password(jesus.password)
+		jesus.save()
+		jesusSeller = Seller(user=jesus, address='Bethlem')
 
-	def 
+		richie = User(first_name='Riche', 
+					 last_name='Ober', 
+					 username='richie',
+					 email='richie@richie.com',
+					 password='asdf1234')
+		jesus.set_password(jesus.password)
+		jesus.save()
+		jesusSeller = Seller(user=jesus, address='Frankfurt an der Oder')
+
+
+class registerUser(TestCase):
+	pass
 
