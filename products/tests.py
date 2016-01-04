b947fef2eb3cd2ef12d97172ab59dffea052ea6c
Jose <joseharoperalta@gmail.com>
2016-01-04 23:41:19 +0100

    Start testing basic functionality in the site

--- products/tests.py
+++ products/tests.py
@@ -1,31 +1,42 @@
 from django.test import TestCase
 from django.test.utils import setup_test_environment
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse, resolve
 
+from .views import IndexView, register, Login, logout_view, LoggedIn, removeProduct, AddProduct, EditProduct
 from .models import Seller, User, Product
 
+class HomePageTest(TestCase):
 
-class CreateSellerTest(TestCase):
-	def setUp(self):
-		jesus = User(first_name='Jesus', 
-					 last_name='Christ', 
-					 username='jesus',
-					 email='jesus@jesus.com',
-					 password='asdf1234')
-		jesus.set_password(jesus.password)
-		jesus.save()
-		jesusSeller = Seller(user=jesus, address='Bethlem')
+	def test_root_url_resolves_to_home_page_view(self):
+		found = resolve('/')
+		self.assertEqual(found, IndexView)
 
-		richie = User(first_name='Riche', 
-					 last_name='Ober', 
-					 username='richie',
-					 email='richie@richie.com',
-					 password='asdf1234')
-		jesus.set_password(jesus.password)
-		jesus.save()
-		jesusSeller = Seller(user=jesus, address='Frankfurt an der Oder')
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = IndexView(request)
 
 
-class registerUser(TestCase):
-	pass
+#class CreateSellerTest(TestCase):
+#	def setUp(self):
+#		jesus = User(first_name='Jesus', 
+#					 last_name='Christ', 
+#					 username='jesus',
+#					 email='jesus@jesus.com',
+#					 password='asdf1234')
+#		jesus.set_password(jesus.password)
+#		jesus.save()
+#		jesusSeller = Seller(user=jesus, address='Bethlem')
+#
+#		richie = User(first_name='Riche', 
+#					 last_name='Ober', 
+#					 username='richie',
+#					 email='richie@richie.com',
+#					 password='asdf1234')
+#		jesus.set_password(jesus.password)
+#		jesus.save()
+#		jesusSeller = Seller(user=jesus, address='Frankfurt an der Oder')
+
+
+#class registerUser(TestCase):
+#	pass
 
